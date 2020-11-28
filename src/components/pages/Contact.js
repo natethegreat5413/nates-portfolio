d75fff60c0cca4b0cc3f2feca77aa33371e406df
Nathan Cowley <nathancowley12@gmail.com>
2020-11-28 13:41:40 -0700

    taking a different route with the contact page, further work needed.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import '../../App.css'
+import axios from 'axios'
+import '../pages/Contact.css'
 
-export default function Contact() {
-    return <h1 className='contact'>Let's Chat!</h1>
-}
+    function Contact(){
+        return(
+            <h1>Let's Talk!</h1>
+        )
+    }
+    
+ {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+ {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
+                
+    
+    export default Contact;
